a3f63e46be504834e56d9530f12a151f79949fe8
wxb820901 <wxb820901@gmail.com>
2017-12-20 16:33:23 +0800

    Create ApplicationTestWithActiveMQ.java

---
+++ gs-accessing-data-rest/src/test/com/bill/ApplicationTestWithActiveMQ.java
@@ -0,0 +1,47 @@
+package com.bill;
+
+import com.bill.entity.Email;
+import org.apache.activemq.broker.BrokerService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTestWithActiveMQ {
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @Before
+    public void deleteAllBeforeTests() throws Exception {
+        jmsTemplate = context.getBean(JmsTemplate.class);
+    }
+
+
+    @Test
+    public void sendSimpleMessage() throws InterruptedException {
+
+        if(context != null){
+            System.out.println(" =======> Sending an email message.");
+            jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+        }
+
+        Thread.sleep(1000L);
+        assertThat(this.outputCapture.toString().contains("info@example.com")).isTrue();
+    }
+}
