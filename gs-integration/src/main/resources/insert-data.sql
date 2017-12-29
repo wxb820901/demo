cbb819622dbfb020a3faa86f417301549b0cf744
wxb820901 <wxb820901@gmail.com>
2017-12-29 15:42:48 +0800

    Create insert-data.sql

---
+++ gs-integration/src/main/resources/insert-data.sql
@@ -0,0 +1,7 @@
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1001', 'Cartman', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1002', 'bill', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1003', 'a', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1004', 'b', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1005', 'c', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1006', 'd', NOW());
+insert into PEOPLE(id, name, CREATED_DATE_TIME) values ('1007', 'e', NOW());
